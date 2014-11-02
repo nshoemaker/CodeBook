2081c9562cd19923e3e73fc2b7bab1ac73c4b80d
Nora <nora@mindcrafted.com>
2014-11-02 00:34:09 -0400

    Setup initial DB so I can start doing template stuff #4. Also working on front page #13.

--- finalproj/codebook/models.py
+++ finalproj/codebook/models.py
@@ -1,3 +1,81 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+from django.db.models import Q
+
+class Stack(models.Model):
+    name = models.CharField(max_length=40)
+
+class Language(models.Model):
+    name = models.CharField(max_length=20)
+    #extensions = models.CharField(max_length=20)
+
+class Rating(models.Model):
+    language = models.ForeignKey(Language)
+    proficiency = models.IntegerField()  # how well they think they know the language
+    credibility = models.IntegerField()  # how well we think they know the language
+
+
+class ProfileUser(models.Model):
+    user = models.OneToOneField(User)
+    following = models.ManyToManyField("self", related_name="followers", blank=True)
+    # can list followers of user and list users followed by another user
+    rating = models.ManyToManyField(Rating, related_name="rated", blank=True)
+    email = models.CharField(max_length=400)
+    website = models.CharField(max_length=400, blank=True)
+    company = models.CharField(max_length=400, blank=True)
+    bio = models.CharField(max_length=400, blank=True)
+
+
+
+class Repository(models.Model):
+    creator = models.ForeignKey(ProfileUser)
+    name = models.CharField(max_length=200)
+    contributors = models.ManyToManyField(ProfileUser, related_name="contributed", blank=True)
+    # sorted by number of commits descending order
+    language = models.ManyToManyField(Language, related_name="language_used_in", blank=True)
+    #number next to each lang is number of bytes of code in that language
+    stack = models.ManyToManyField(Stack, related_name="stack_used_in", blank=True)
+    #commitActivityData = array of array of ints, may want to save this
+    date_created = models.DateTimeField(auto_now_add=False)
+    star_count = models.IntegerField()
+    fork_count = models.IntegerField()
+    kb_size = models.IntegerField()
+
+class Tag(models.Model):
+    message = models.CharField(max_length=20)
+    profile_user = models.ForeignKey(ProfileUser, related_name="created")
+    repository = models.ForeignKey(Repository)
+    date_time = models.DateTimeField(auto_now_add=True)
+    endorsements = models.ManyToManyField(ProfileUser)
+    # tagger field with hash info about tagger - name, email, date
+
+class Comment(models.Model):
+    profile_user = models.ForeignKey(ProfileUser)
+    body = models.CharField(max_length=400)  # text of comment
+    repository = models.ForeignKey(Repository)
+    date_time = models.DateTimeField(auto_now_add=True)
+    path = models.CharField(max_length=400)  # relative path file comment on
+    likes = models.IntegerField()
+
+
+
+class Difficulty(models.Model):
+    rating = models.IntegerField()
+    repository = models.ForeignKey(Repository)
+    profile_user = models.ForeignKey(ProfileUser)
+    date_time = models.DateTimeField(auto_now_add=True)
+
+
+class Watch(models.Model):
+    # can list watchers and list repositories being watched
+    profile_user = models.ForeignKey(ProfileUser)
+    repository = models.ForeignKey(Repository)
+    date_time = models.DateTimeField(auto_now_add=True)
+
+class Star(models.Model):
+    # can list stargazers and list repositories being starred
+    profile_user = models.ForeignKey(ProfileUser)
+    repository = models.ForeignKey(Repository)
+    date_time = models.DateTimeField(auto_now_add=True)
--- finalproj/codebook/views.py
+++ finalproj/codebook/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.db import transaction
 
-# Create your views here.
+from django.core.exceptions import ObjectDoesNotExist
+
+# Needed to manually create HttpResponses or raise an Http404 exception
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+
+# Decorator to use built-in authentication system
+from django.contrib.auth.decorators import login_required
+
+# Used to create and manually log in a user
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+
+# Used to generate a one-time-use token to verify a user's email address
+from django.contrib.auth.tokens import default_token_generator
+
+# Used to send mail from within Django
+from django.core.mail import send_mail
+
+# Helper function to guess a MIME type from a file name
+from mimetypes import guess_type
+
+from django.db.models import Q
+
+#from codebook.models import *
+#from codebook.forms import *
+
+from datetime import datetime
+from django.utils import timezone
+
+import json
+
+def front(request):
+    context = {}
+
+    return render(request, 'codebook/front-page.html', context)
+
