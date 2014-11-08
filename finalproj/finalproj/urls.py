77a43f6286ff010e0317becc5aa00055242b42ab
Nora <nora@mindcrafted.com>
2014-11-08 17:05:18 -0500

    Getting rid of views.py

--- finalproj/finalproj/urls.py
+++ finalproj/finalproj/urls.py
@@ -2,11 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'finalproj.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^codebook/', include('codebook.urls')),
-    url(r'^$', 'codebook.views.front'),
+    url(r'^$', 'codebook.content_views.front'),
     url(r'^admin/', include(admin.site.urls)),
 )
