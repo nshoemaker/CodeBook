ff70a1384bcdf89e3052e4c7a828248d08ea5b37
Nora <nora@mindcrafted.com>
2014-11-08 12:23:17 -0500

    All language icons now change color upon mouseover.

---
+++ finalproj/codebook/static/js/language_buttons.js
@@ -0,0 +1,169 @@
+/**
+ * Created by nora on 11/8/2014.
+ */
+$(document).ready(function()
+{
+    $(".icon-prog-java").mouseover(function()
+    {
+        $(".icon-prog-java").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-java").mouseout(function()
+    {
+        $(".icon-prog-java").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-c").mouseover(function()
+    {
+        $(".icon-prog-c").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-c").mouseout(function()
+    {
+        $(".icon-prog-c").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-js02").mouseover(function()
+    {
+        $(".icon-prog-js02").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-js02").mouseout(function()
+    {
+        $(".icon-prog-js02").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-ruby").mouseover(function()
+    {
+        $(".icon-prog-ruby").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-ruby").mouseout(function()
+    {
+        $(".icon-prog-ruby").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-rails").mouseover(function()
+    {
+        $(".icon-prog-rails").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-rails").mouseout(function()
+    {
+        $(".icon-prog-rails").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-nodejs02").mouseover(function()
+    {
+        $(".icon-prog-nodejs02").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-nodejs02").mouseout(function()
+    {
+        $(".icon-prog-nodejs02").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-python").mouseover(function()
+    {
+        $(".icon-prog-python").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-python").mouseout(function()
+    {
+        $(".icon-prog-python").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-django").mouseover(function()
+    {
+        $(".icon-prog-django").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-django").mouseout(function()
+    {
+        $(".icon-prog-django").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-php02").mouseover(function()
+    {
+        $(".icon-prog-php02").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-php02").mouseout(function()
+    {
+        $(".icon-prog-php02").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-cplusplus").mouseover(function()
+    {
+        $(".icon-prog-cplusplus").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-cplusplus").mouseout(function()
+    {
+        $(".icon-prog-cplusplus").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-csharp").mouseover(function()
+    {
+        $(".icon-prog-csharp").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-csharp").mouseout(function()
+    {
+        $(".icon-prog-csharp").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-objc").mouseover(function()
+    {
+        $(".icon-prog-objc").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-objc").mouseout(function()
+    {
+        $(".icon-prog-objc").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-matlab").mouseover(function()
+    {
+        $(".icon-prog-matlab").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-matlab").mouseout(function()
+    {
+        $(".icon-prog-matlab").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-aspnet").mouseover(function()
+    {
+        $(".icon-prog-aspnet").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-aspnet").mouseout(function()
+    {
+        $(".icon-prog-aspnet").css("background-color", "#55BB88");
+    });
+
+
+    $(".icon-prog-bash02").mouseover(function()
+    {
+        $(".icon-prog-bash02").css("background-color", "#77DDAA");
+    });
+
+    $(".icon-prog-bash02").mouseout(function()
+    {
+        $(".icon-prog-bash02").css("background-color", "#55BB88");
+    });
+});
