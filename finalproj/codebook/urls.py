f72d43bfe4b62d73b7a0bded86e724eac49906d2
Divya Mouli <dmouli@andrew.cmu.edu>
2014-11-08 15:29:29 -0500

    Fixed urls.py for new views

--- finalproj/codebook/urls.py
+++ finalproj/codebook/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
 
 	# Loading Content
-    url(r'^$', 'codebook.views.front', name='front'),
-    url(r'^news$', 'codebook.views.news', name='news'),
-    url(r'^watching$', 'codebook.views.watching', name='watching'),
-    url(r'^starred$', 'codebook.views.starred', name='starred'),
-    url(r'^following$', 'codebook.views.following', name='following'),
-    url(r'^signin$', 'codebook.views.signin', name='signin'),
-    url(r'^signup$', 'codebook.views.signup', name='signup'),
+    url(r'^$', 'codebook.content_views.front', name='front'),
+    url(r'^news$', 'codebook.content_views.news', name='news'),
+    url(r'^watching$', 'codebook.content_views.watching', name='watching'),
+    url(r'^starred$', 'codebook.content_views.starred', name='starred'),
+    url(r'^following$', 'codebook.content_views.following', name='following'),
+    url(r'^signin$', 'codebook.content_views.signin', name='signin'),
+    url(r'^signup$', 'codebook.content_views.signup', name='signup'),
 
 
     # User Actions 
