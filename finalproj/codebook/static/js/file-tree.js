2cbe1a964a5cf58ca979e85f1be873fb6eb75bb3
Nora <nora@mindcrafted.com>
2014-11-16 17:50:52 -0500

    base.html should not include anything in the search-panel block

--- finalproj/codebook/static/js/file-tree.js
+++ finalproj/codebook/static/js/file-tree.js
@@ -2,7 +2,7 @@
  * Created by nora on 11/15/2014.
  */
 $(document).ready( function() {
-    $('#container_id').fileTree({ root: '/some/folder/' }, function(file) {
+    $('#contagfhgjhkjliner_id').fileTree({ root: '/some/folder/' }, function(file) {
         alert(file);
     });
 });
