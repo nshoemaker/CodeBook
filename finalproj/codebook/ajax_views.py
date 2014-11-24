95909f05e7d40457327cf10e94e996b6eeeddebd
Divya Mouli <dmouli@andrew.cmu.edu>
2014-11-24 17:06:29 -0500

    Cleaned up views, created views_base that all views files will import, and moved Repo class to views_base so it can be accessed by all views files

--- finalproj/codebook/ajax_views.py
+++ finalproj/codebook/ajax_views.py
@@ -1,98 +1,13 @@
-__author__ = 'nora'
-from content_views import *
-from user_action_views import *
-from django.shortcuts import render, redirect, get_object_or_404
-from django.core.urlresolvers import reverse
-from django.db import transaction
+##############################################################
+##############################################################
+##  ajax_views.py:                                          ##
+##                                                          ##
+##  Contains actions which implement different user actions ##
+##      with ajax calls for quick loading                   ##
+##############################################################
+##############################################################
+from views_base import *
 
-from django.core.exceptions import ObjectDoesNotExist
-
-# Needed to manually create HttpResponses or raise an Http404 exception
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.shortcuts import render_to_response
-
-
-# Decorator to use built-in authentication system
-from django.contrib.auth.decorators import login_required
-
-# Used to generate a one-time-use token to verify a user's email address
-from django.contrib.auth.tokens import default_token_generator
-
-# Used to send mail from within Django
-from django.core.mail import send_mail
-
-# Helper function to guess a MIME type from a file name
-from mimetypes import guess_type
-
-from django.db.models import Q
-
-from codebook.models import *
-from codebook.forms import *
-
-from datetime import datetime
-from django.utils import timezone
-
-import json
-import sys
-import os
-"""
-To use this class: 
-Initialize as Repo(None,Repository.repo_id,user) if want to create from database model repository. 
-Initialize as Repo(githubrepo,0,user) if want to create from github repo.
-User is github user, get by g.get_user() of authenticated g user.
-"""
-class Repo:
-    def __init__(self, repo, id, user, hub):
-        if(repo is None):
-            repo = hub.get_repo(id)
-            self.comments = Comment.objects.filter(repository__repo_id = repo.id)
-        else:
-            self.comments = Comment.objects.none()
-        branches = repo.get_branches()
-        SHA = branches[0].commit.sha
-        tree = repo.get_git_tree(SHA).tree
-        self.default_file_name = ""
-        self.default_file_contents = ""
-        self.default_file_path = ""
-        self.file_tree = None
-        try:
-            branches = repo.get_branches()
-            SHA = branches[0].commit.sha
-            tree = repo.get_git_tree(SHA,False).tree
-            self.file_tree = tree
-            for i in xrange(len(tree)):
-                deffile = repo.get_contents(tree[i].path)
-                if deffile.type == 'file':
-                    self.default_file_name = deffile.name
-                    self.default_file_contents = base64.b64decode(deffile.content)
-                    self.default_file_path = deffile.path
-                    break
-            self.readme = None
-            self.readme_contents = ""
-            #check first thing blob type
-        except:
-            pass
-        self.id = repo.id
-        self.name = repo.name
-        self.description = repo.description
-        self.url = repo.html_url
-        self.langs = repo.language
-        self.org = repo.organization
-        self.owner_name = repo.owner.login
-        self.owner_prof_pic = repo.owner.avatar_url
-        self.is_current_user_starring = user.has_in_starred(repo) 
-        self.star_count = repo.stargazers_count
-        self.is_current_user_watching = user.has_in_subscriptions(repo) 
-        self.watch_count = repo.watchers_count
-        self.doc_rating = 0
-        self.difficulty_rating = 0
-        self.tag_list = None
-"""      except Exception,e:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print(exc_type, fname, exc_tb.tb_lineno) 
-            print str(e)
-"""
 def get_formatted_nodes(self, tree, repo):
     print tree
     print " "
--- finalproj/codebook/content_views.py
+++ finalproj/codebook/content_views.py
@@ -1,112 +1,24 @@
-############################################################
-############################################################
-##  content_views.py:                                     ##
-##                                                        ##
-##  Contains actions which populate pages of the web app  ##
-##  Actions:                                              ##
-##      front     - populates front-page.html             ##
-##      news      - populates news-page.html              ##
-##      watching  - populates watching-page.html          ##
-##      starred   - populates starred-page.html           ##
-##      following - populates following-page.html         ##
-############################################################
-############################################################
+##############################################################
+##############################################################
+##  content_views.py:                                       ##
+##                                                          ##
+##  Contains actions which populate pages of the web app    ##
+##  Actions:                                                ##
+##      front           - populates front page              ##
+##      news            - populates my languages stream     ##
+##      watching        - populates watch stream            ##
+##      saved           - populates saved stream            ##
+##      following       - populates following-page.html     ##
+##      my_profile_view - populates view of own profile     ##
+##      profile_view    - populates view of a profile page  ##
+##                                                          ##
+##############################################################
+##############################################################
 
-from django.shortcuts import render, redirect, get_object_or_404
-from django.core.urlresolvers import reverse
-from django.db import transaction
-
-from django.core.exceptions import ObjectDoesNotExist
-
-from github import Github 
-# g = Github(user, password) - USE THIS ONE TO TEST B/C IT WON'T HIT RATE LIMIT
+from views_base import *
 g = Github('dmouli', 'Spongebob5%')
 """
 g = Github(token)
-"""
-
-# Needed to manually create HttpResponses or raise an Http404 exception
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-
-# Decorator to use built-in authentication system
-from django.contrib.auth.decorators import login_required
-
-# Used to create and manually log in a user
-from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
-
-# Used to generate a one-time-use token to verify a user's email address
-from django.contrib.auth.tokens import default_token_generator
-
-# Used to send mail from within Django
-from django.core.mail import send_mail
-
-from django.template.context import RequestContext
-
-# Helper function to guess a MIME type from a file name
-from mimetypes import guess_type
-
-from django.db.models import Q
-
-from codebook.models import *
-from codebook.forms import *
-
-from datetime import datetime
-from django.utils import timezone
-
-import json
-
-class Repo:
-    def __init__(self, repo, id, user, hub):
-        if(repo is None):
-            repo = hub.get_repo(id)
-            self.comments = Comment.objects.filter(repository__repo_id = repo.id)
-        else:
-            self.comments = Comment.objects.none()
-        branches = repo.get_branches()
-        SHA = branches[0].commit.sha
-        tree = repo.get_git_tree(SHA).tree
-        self.default_file_name = ""
-        self.default_file_contents = ""
-        self.default_file_path = ""
-        self.file_tree = None
-        try:
-            branches = repo.get_branches()
-            SHA = branches[0].commit.sha
-            tree = repo.get_git_tree(SHA,False).tree
-            self.file_tree = tree
-            for i in xrange(len(tree)):
-                deffile = repo.get_contents(tree[i].path)
-                if deffile.type == 'file':
-                    self.default_file_name = deffile.name
-                    self.default_file_contents = base64.b64decode(deffile.content)
-                    self.default_file_path = deffile.path
-                    break
-            self.readme = None
-            self.readme_contents = ""
-            #check first thing blob type
-        except:
-            pass
-        self.id = repo.id
-        self.name = repo.name
-        self.description = repo.description
-        self.url = repo.html_url
-        self.langs = repo.language
-        self.org = repo.organization
-        self.owner_name = repo.owner.login
-        self.owner_prof_pic = repo.owner.avatar_url
-        self.is_current_user_starring = user.has_in_starred(repo) 
-        self.star_count = repo.stargazers_count
-        self.is_current_user_watching = user.has_in_subscriptions(repo) 
-        self.watch_count = repo.watchers_count
-        self.doc_rating = 0
-        self.difficulty_rating = 0
-        self.tag_list = None
-"""      except Exception,e:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print(exc_type, fname, exc_tb.tb_lineno) 
-            print str(e)
 """
 
 def my_profile_view(request):
@@ -127,12 +39,6 @@ def profile_view(request, username):
         context["view_my_profile"] = 'true'
     context['ratings'] = UserRating.objects.filter(profile_user = profile_user)
     return render(request, 'codebook/profile.html', context)
-
-def get_auth_user_git(request):
-    social = request.user.social_auth.get(provider='github')
-    token = social.extra_data['access_token']
-    g = Github(token)
-    return g
 
 def saved(request):
     context = {}
@@ -192,17 +98,6 @@ def watching(request):
     context['profile_user'] = request.user
     context['gh_user'] = user
     return render(request, 'codebook/watching-page.html', context)
-
-
-#@login_required
-def following(request):
-    context = {}
-    # TODO: this is just temporary. Replace with actual list of user ids of people the user is following (max 10)
-    following_list_short = ['1','2','3','4','5','6','7','8','9','10','11','12']
-    context['following_list_short'] = following_list_short
-    context['searchform'] = SearchForm()
-    #context['profile_user'] = ProfileUser.objects.get(user=request.user)
-    return render(request, 'codebook/following-page.html', context)
 
 
 def sandbox(request):
