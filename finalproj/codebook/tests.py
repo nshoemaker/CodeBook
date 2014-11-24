8ab930ca271c74d6658b73693f689f27bb04537f
Nora <nora@mindcrafted.com>
2014-11-24 09:03:24 -0500

    Added a few example tests

--- finalproj/codebook/tests.py
+++ finalproj/codebook/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
-
+# TO RUN THE TESTS :
+# manage.py test codebook.tests
 # Create your tests here.
+
+from django.test import TestCase
+from django.test import Client
+
+class Responses_200(TestCase):
+    def SetUp(self):
+        self.client = Client()
+
+    def test_response_200(self):
+        #response_admin = self.client.get('/admin/')
+        #self.assertEqual(response_admin.status_code, 200)
+
+        response = self.client.get('/codebook/')
+        print "Testing /codebook/ not logged in"
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/codebook/news')
+        print "Testing /codebook/news not logged in"
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/codebook/watching')
+        print "Testing /codebook/news/watching not logged in"
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/codebook/following')
+        print "Testing /codebook/news/following not logged in"
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/codebook/saved')
+        print "Testing /codebook/news/saved not logged in"
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/codebook/profile/nshoemaker')
+        print "Testing /codebook/news/profile/nshoemaker not logged in"
+        self.assertEqual(response.status_code, 200)
