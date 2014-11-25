8d34701f417c7c1824d719aea02d749bac4f63e0
Nora <nora@mindcrafted.com>
2014-11-25 15:54:18 -0500

    static urls now available

--- finalproj/finalproj/urls.py
+++ finalproj/finalproj/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     url(r'^codebook/', include('codebook.urls')),
@@ -14,4 +15,4 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns += patterns('',
         url(r'^__debug__/', include(debug_toolbar.urls)),
-    )
+    ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
