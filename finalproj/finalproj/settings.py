1717441115efa14feb0ee9237ffa5673ebd96771
Nora <nora@mindcrafted.com>
2014-11-25 16:14:52 -0500

    Adding new github keys for the deployed app

--- finalproj/finalproj/settings.py
+++ finalproj/finalproj/settings.py
@@ -68,8 +68,13 @@ AUTHENTICATION_BACKENDS = (
    'django.contrib.auth.backends.ModelBackend',
 )
 
-SOCIAL_AUTH_GITHUB_KEY = '31fd927b2ac22678d050'
-SOCIAL_AUTH_GITHUB_SECRET = '431197c65d51dc0ae5e4919fc44a999b95e2fef3'
+# these are the settings for local development
+#SOCIAL_AUTH_GITHUB_KEY = '31fd927b2ac22678d050'
+#SOCIAL_AUTH_GITHUB_SECRET = '431197c65d51dc0ae5e4919fc44a999b95e2fef3'
+
+# new keys for deployment:
+SOCIAL_AUTH_GITHUB_KEY = 'aca57928bf5933e6f19e'
+SOCIAL_AUTH_GITHUB_SECRET = '477759c2ab4260dcb708c1c719a370d316891840'
 SOCIAL_AUTH_GITHUB_SCOPE = ['repo']
 SOCIAL_AUTH_USER_MODEL = 'codebook.ProfileUser'
 
