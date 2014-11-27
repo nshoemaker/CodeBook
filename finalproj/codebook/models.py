32d6916452f285f796b679404e9d1cb0866593d8
Nora <nora@mindcrafted.com>
2014-11-26 21:51:51 -0500

    Don't need to have language name as primary key for now

--- finalproj/codebook/models.py
+++ finalproj/codebook/models.py
@@ -17,7 +17,7 @@ class Stack(models.Model):
     icon = models.CharField(max_length=40)
 
 class Language(models.Model):
-    name = models.CharField(max_length=20, primary_key=True)
+    name = models.CharField(max_length=20)
     #extensions = models.CharField(max_length=20)
 
 class ProfileUser(AbstractBaseUser):
