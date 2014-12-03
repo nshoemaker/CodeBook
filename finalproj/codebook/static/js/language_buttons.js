79a74fd9b48ac2d53bfc6e8ba16c2a88c5114c4a
Nora <nora@mindcrafted.com>
2014-12-03 03:39:46 -0500

    Added CSS icon to language buttons

--- finalproj/codebook/static/js/language_buttons.js
+++ finalproj/codebook/static/js/language_buttons.js
@@ -231,15 +231,28 @@ $(document).ready(function()
     $(".icon-orkut").mouseover(function()
     {
         $(this).css("background-color", "#77DDAA");
-        //$(".icon-orkut").css("background-color", "#77DDAA");
-        $("#language-choice").text("Bash");
+        $("#language-choice").text("a Language");
         $("#language-choice").css("font-weight","bolder");
     });
 
     $(".icon-orkut").mouseout(function()
     {
         $(this).css("background-color", "#55BB88");
-        //$(".icon-orkut").css("background-color", "#55BB88");
+        $("#language-choice").text("a Language");
+        $("#language-choice").css("font-weight","normal");
+    });
+
+
+    $(".icon-css3-01").mouseover(function()
+    {
+        $(this).css("background-color", "#77DDAA");
+        $("#language-choice").text("CSS");
+        $("#language-choice").css("font-weight","bolder");
+    });
+
+    $(".icon-css3-01").mouseout(function()
+    {
+        $(this).css("background-color", "#55BB88");
         $("#language-choice").text("a Language");
         $("#language-choice").css("font-weight","normal");
     });
